211e6f9f8fc52032c5f118ef0f811b85d9c32f1f
Prachi <1998prachi14@gmai.com>
2026-03-27 12:59:25 +0530

    basics

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail="prachi@example.com"
+var accountPassword="12345"
+accountCity="Prg"
+
+// accountId=2 not allowed as accountId is a constant variable
+
+accountEmail= "ppp@pp.com"
+accountPassword="21212121"
+accountCity="Ald"
+let accountstate;
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
